0349f42fdb4d0a43374bfd324c1e61bbcc931a41
calefrey <calefrey@gmail.com>
2018-04-21 16:47:20 -0400

    Json Conversion

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask, render_template, request
 from song import Song
+import json
 import spotify
 app = Flask(__name__)
 
@@ -11,10 +12,11 @@ def index():
 def search():
     if request.method == 'POST':
         query = request.form['SongSearch']
-        print(query)
         url = 'https://api.spotify.com/v1/search'
-        payload = {'q':query, 'type':'track', 'market':'US', 'limit':5}
-        print(requests.get(url, params = payload, headers = spotify.auth()).content)
-    return render_template('search.html') 
+        payload = {'q':query, 'type':'track', 'market':'US', 'limit':1}
+        r = requests.get(url, params = payload, headers = spotify.auth())
+        spotify.saveResults(r.json())
+    return render_template('search.html')
+
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port=8000, debug=True)
--- spotify.py
+++ spotify.py
@@ -1,8 +1,24 @@
 import secrets
+from song import Song
+import json
 def auth():
     header = {'Authorization': 
         'Bearer {}'.format(secrets.access_token)
         }
-    print(header)
     return header
-    
+
+def saveResults(r):
+    results = []
+    json = r
+    songs=json['tracks']['items']
+    for i in range(0,5):
+        uri = songs[i]['id']
+        album = songs[i]['album']['name']
+        artist = songs[i]['artists']['name']
+        title = songs[i]['name']
+        results[i] = Song(title, album, artist, uri)
+        print(uri)
+        print(album)
+        print(artist)
+        print(title)
+    return results
