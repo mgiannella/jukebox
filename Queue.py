7a1aa5475dac1e12332b8762d7aab38e7256317e
Adam Romano <romano@nwk-148-205.nwk.ruw.rutgers.edu>
2018-04-21 18:48:28 -0400

    Queue

--- Queue.py
+++ Queue.py
@@ -4,18 +4,22 @@ class Queue:
     def __init__(self): 
         self.queue = []
         self.size = 0
+        self.nowPlaying = None
 
     def add(self, Song): 
-        duplicate = False
-        for item in self.queue: 
-            song_uri = item.info['uri']
-            if Song.info['uri'] == song_uri: 
-                item.info['score'] += 1
-                duplicate = True
-        if duplicate == False: 
-            self.queue.append(Song)
-            self.size +=1 
-        self.queue.sort(key = lambda s: s.info['score'], reverse = True)
+        if self.size == 0: 
+            self.nowPlaying = Song
+        else: 
+            duplicate = False
+            for item in self.queue: 
+                song_uri = item.info['uri']
+                if Song.info['uri'] == song_uri: 
+                    item.info['score'] += 1
+                    duplicate = True
+            if duplicate == False: 
+                self.queue.append(Song)
+                self.size +=1 
+            self.queue.sort(key = lambda s: s.info['score'], reverse = True)
     
     def get(self): 
         return self.queue[0]
